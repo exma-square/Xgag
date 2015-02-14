643d0f3f10172349b8b9083f2d6d0acc87ecd2f6
Simon Sun <blackghost0118@gmail.com>
2015-02-14 23:55:35 +0800

    修改上傳路徑錯誤問題

--- app.js
+++ app.js
@@ -43,15 +43,15 @@ app.post('/create', controllers.users.create);
 app.post('/login', controllers.users.login);
 // 登出
 app.get('/logout', controllers.users.logout);
-// app.post('/upload', multipart(), controllers.users.upload)
-app.post('/upload', multipart(), function(req, res){
-  var filename = req.files.image.originalFilename || path.basename(req.files.image.ws.path);
-  var targetPath = path.dirname(__filename) + '/public/' + filename;
-  //copy file
-  fs.createReadStream(req.files.image.ws.path).pipe(fs.createWriteStream(targetPath));
-  //return file url
-  res.json({code: 200, msg: {url: 'http://' + req.headers.host + '/' + filename}});
-});
+app.post('/upload', multipart(), controllers.users.upload);
+// app.post('/upload', multipart(), function(req, res){
+//   var filename = req.files.image.originalFilename || path.basename(req.files.image.ws.path);
+//   var targetPath = path.dirname(__filename) + '/public/' + filename;
+//   //copy file
+//   fs.createReadStream(req.files.image.ws.path).pipe(fs.createWriteStream(targetPath));
+//   //return file url
+//   res.json({code: 200, msg: {url: 'http://' + req.headers.host + '/' + filename}});
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/users/upload.js
+++ controllers/users/upload.js
@@ -1,9 +1,10 @@
 var fs = require('fs');
 var path = require('path');
+var rootPath = require('app-root-path').path;
 
 module.exports = function (req, res){
   var filename = req.files.image.originalFilename || path.basename(req.files.image.ws.path);
-  var targetPath = path.dirname(__filename) + '/public/' + filename;
+  var targetPath = rootPath + '/public/' + filename;
   //copy file
   fs.createReadStream(req.files.image.ws.path).pipe(fs.createWriteStream(targetPath));
   //return file url
