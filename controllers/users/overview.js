b58dde516e56d5011178e838ede4a12fce124702
webber_wu <webber_wu@hiiir.com>
2015-02-16 21:16:51 +0800

    差滾動去讀取資料庫的infinitescroll

--- controllers/users/overview.js
+++ controllers/users/overview.js
@@ -1,4 +1,11 @@
+var models = require('../../models');
+
 module.exports = function (req, res){
-  posts = null;
-  res.render('user.jade', { title: 'OVERVIEW', user: req.session.user, posts: posts || 0 });
+  models.posts.find({name: req.session.user.name}).sort({create_date:-1}, function(err, posts){
+    if(err){
+      console.error(err);
+      return res.send(err);
+    }
+    res.render('user.jade', { title: 'OVERVIEW', user: req.session.user, posts: posts || 0 });
+  });
 };
