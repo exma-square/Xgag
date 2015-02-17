7ac75d07a50f67371d063ad53dfe6b4eb07658ea
clonn <clonncd@gmail.com>
2015-02-17 23:02:04 +0800

    update model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 var mongojs = require('mongojs'),
-  db = mongojs('mongodb://localhost:27017/xgag'),
+  dbURI = (process.env.NODE_ENV === "production") ? process.env.MONGO_URI : 'mongodb://localhost:27017/xgag',
+  db = mongojs(dbURI),
   users = db.collection('users'),
   posts = db.collection('posts')
 
