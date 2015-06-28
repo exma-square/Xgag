17ea67661d401743a33a626ae89e9a2a3c375000
clonn <clonncd@gmail.com>
2015-06-28 18:52:22 +0800

    add like and dislike to front end

--- controllers/root/home.js
+++ controllers/root/home.js
@@ -2,6 +2,8 @@ module.exports = function (req, res){
   if (req.session.flash.length > 0) {
     delete req.session.flash;
   }
+
+  console.log(req.session.user);
   res.render('index.jade', { title: 'Xgag', user: req.session.user });
 
 };
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,4 @@
 $(function(){
-
   $.get( "/getPosts", function( data ) {
     $("#contentTmpl").tmpl(data.posts).appendTo(".post-clump");
     $("img").error(function () {
@@ -13,5 +12,25 @@ $(function(){
       var string = "<img style='width:100%' src='${newspic}'/><h4>${title}</h4><p>${content}</p>";
       $("#news-prewiew").html($.tmpl( string, data ));
     });
+  });
+
+  $(".container" ).delegate(".motion", "click", function(e) {
+    e.preventDefault();
+    var target = $(e.currentTarget);
+    console.log(e);
+    var url = target.attr("href");
+    var id = url.split("/").pop();
+    $.get(target.attr("href"), function (result) {
+      
+      if (result.code !== 200) {
+        console.log(result);
+        return alert("Please login and try again");  
+      }
+      var inTarget = (url.indexOf("dislike") > -1 ) ? "dislike" : "like";
+      var actionTarget = $("#" + id + " ." + inTarget);
+      actionTarget.html(parseInt(actionTarget.html(), 10) + 1);
+      return;
+    })
+
   });
 });
