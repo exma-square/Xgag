53d91bf60b7f40d42f06ae01a2a6acab8015b872
webber_wu <webber_wu@hiiir.com>
2015-07-27 10:19:39 +0800

    區分 error middlewares 區塊, 把 www用不到的code拿掉

---
+++ middlewares/index.js
@@ -0,0 +1,32 @@
+var express      = require('express');
+var flash        = require('flash');
+var logger       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('cookie-session');
+var stylus       = require('stylus');
+var nib          = require('nib');
+
+module.exports = function(app) {
+
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(session({
+    key: 'sessionId',
+    secret: 'session_cookie_secret+sdjfiawe958723',
+    cookie: {
+      maxAge: 1000 * 60
+    }
+  }));
+  app.use(flash());
+  app.use(stylus.middleware({
+    src: __dirname + '/public/stylus',       // .styl files are located in `views/stylesheets`
+    dest: __dirname + '/public/stylesheets', // .styl resources are compiled `/stylesheets/*.css`
+
+    compile : function(str, path) {
+      return stylus(str).set('filename', path).set('compress', true).use(nib());
+    }
+  }));
+};
