1730b7e78fcaa776dc28c5f1b8352f403c2db572
webber_wu <webber_wu@hiiir.com>
2015-07-29 09:38:36 +0800

    route postDetail

--- app.js
+++ app.js
@@ -61,6 +61,7 @@ app.get('/logout', controllers.users.logout);
 app.post('/upload', multipart(), controllers.users.upload);
 // 貼文
 app.get('/getPosts', controllers.posts.getPosts);
+app.get('/detailPost/:id', controllers.posts.detailPost);
 // like and dislike
 app.get('/like/add/:id', controllers.posts.addLike);
 app.get('/dislike/add/:id', controllers.posts.addDislike);
