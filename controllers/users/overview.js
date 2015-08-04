93a9ab1bd6036ff7f65b3114af2e288ab249f350
Billyking <billyking19920205@gmail.com>
2015-08-04 22:06:28 +0800

    取google user name

--- controllers/users/overview.js
+++ controllers/users/overview.js
@@ -1,7 +1,7 @@
 var models = require('../../models');
 
 module.exports = function (req, res){
-  models.posts.find({name: req.session.user.name}).sort({create_date:-1}, function(err, posts){
+  models.posts.find({name: req.user.google.name}).sort({create_date:-1}).exec(function(err, posts){
     if(err){
       console.error(err);
       return res.send(err);
