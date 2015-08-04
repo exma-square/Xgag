e90166923c59e22eaddb16550d8e1cad2d9fc5e2
webber_wu <webber_wu@hiiir.com>
2015-08-04 21:39:37 +0800

    首頁可以work !!

--- middlewares/index.js
+++ middlewares/index.js
@@ -5,11 +5,11 @@ var bodyParser   = require('body-parser');
 var session      = require('cookie-session');
 var stylus       = require('stylus');
 var nib          = require('nib');
-var passport = require('passport');
+var passport     = require('passport');
 
 module.exports = function(app) {
 
-  app.use(favicon(__dirname + '../public/images/error/error.png'));
+  // app.use(favicon(__dirname + '../public/images/error/error.png'));
   app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
--- models/post.js
+++ models/post.js
@@ -4,44 +4,19 @@ var mongoose = require('mongoose');
 var bcrypt   = require('bcrypt-nodejs');
 
 // define the schema for our user model
-var userSchema = mongoose.Schema({
-
-    local            : {
-        email        : String,
-        password     : String,
-        name         : String
-    },
-    facebook         : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    },
-    twitter          : {
-        id           : String,
-        token        : String,
-        displayName  : String,
-        username     : String
-    },
-    google           : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    }
-
+var postSchema = mongoose.Schema({
+    name        : String,
+    title       : String,
+    newsTitle   : String,
+    image       : String,
+    create_date : Date,
+    content     : String,
+    url         : String,
+    site_name   : String,
+    like        : Array,
+    dislike     : Array,
+    percent     : String
 });
 
-// methods ======================
-// generating a hash
-userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-// checking if password is valid
-userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
-};
-
 // create the model for users and expose it to our app
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Post', postSchema);
