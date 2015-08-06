c523cbd6dc065ff30188f24707809c96241e1f79
webber_wu <webber_wu@hiiir.com>
2015-08-06 09:38:59 +0800

    修復頁面沒有顯示使用者名稱的bug

--- controllers/users/overview.js
+++ controllers/users/overview.js
@@ -1,11 +1,11 @@
 var models = require('../../models');
 
 module.exports = function (req, res){
-  models.posts.find({name: req.session.user}).sort({create_date:-1}).exec(function(err, posts){
+  models.post.find({name: req.user.google.name}).sort({create_date:-1}).exec(function(err, posts){
     if(err){
       console.error(err);
       return res.send(err);
     }
-    res.render('user.jade', { title: 'OVERVIEW', user: req.session.user, posts: posts || 0 });
+    res.render('user.jade', { title: 'OVERVIEW', user: req.user, posts: posts || 0 });
   });
 };
