0f1c5b7fab6d4de02ffa832c12fe549bc6374e0b
Billyking <billyking19920205@gmail.com>
2015-08-06 12:22:27 +0800

    facebook 登入完成

--- config/passport.js
+++ config/passport.js
@@ -159,7 +159,8 @@ module.exports = function(passport) {
         // pull in our app id and secret from our auth.js file
         clientID        : configAuth.facebookAuth.clientID,
         clientSecret    : configAuth.facebookAuth.clientSecret,
-        callbackURL     : configAuth.facebookAuth.callbackURL
+        callbackURL     : configAuth.facebookAuth.callbackURL,
+        profileFields   : ['id', 'displayName' , 'emails']
 
     },
 
@@ -185,11 +186,13 @@ module.exports = function(passport) {
                     var newUser            = new User();
 
                     // set all of the facebook information in our user model
-                    console.log(profile);
+                    console.log("profile",profile);
+                    console.log("token",token);
+                    console.log("refreshToken",refreshToken);
                     newUser.facebook.id    = profile.id; // set the users facebook id                   
                     newUser.facebook.token = token; // we will save the token that facebook provides to the user                    
                     newUser.facebook.name  = profile.displayName; // look at the passport user profile to see how names are returned
-                    //newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+                    newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
 
                     // save our user to the database
                     newUser.save(function(err) {
