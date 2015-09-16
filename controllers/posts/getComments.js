266035b7cd829cf47546a88a76e786a949108a7c
Billyking <billyking19920205@gmail.com>
2015-09-17 00:46:29 +0800

    查兩次了資料不知道要怎麼替換QQ

--- controllers/posts/getComments.js
+++ controllers/posts/getComments.js
@@ -3,20 +3,27 @@ var objectIdSelect = mongoose.Types.ObjectId;
 
 module.exports = function (req, res){
   var id = req.params["id"].replace(/id=/g, "");
-  var newComment = new models.comment;
-  models.post.findOne({_id: objectIdSelect(id)}).exec(function(err, post){
+  models.post.findOne({_id: objectIdSelect(id)})
+  .populate('comment')
+  .exec(function(err, post){
     if (err)
       return res.json({ code: 500, message: "id is not found" });
     post = JSON.parse(JSON.stringify(post));
-    models.comment.find({
-      '_id': { $in: post.comment}
-    })
-    .populate('user.user' , 'id' , null)
-    .exec(function(err, comment){
-      console.log(comment);
+    console.log("Post:" , post);
+    users = post.comment.map(function(obj){
+      return obj.user;
+    });
+    console.log(users);
+    for(user in users){
+      models.user.findOne({'user.id':users[user]})
+      .exec(function(err,user_info){
+        console.log(user_info.user);
+      });
+    }
       if (err)
         return res.json({ code: 500, message: "id is not found" });
-      return res.json({comment: comment});
-    });
+    // models.user.find({id: post.comment})
+      return res.json({comment: post.comment , user: user});
+
   });
 };
