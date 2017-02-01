742d76846835d43a77ba9873370f150d07718a51
zpeng <cfps91152@gmail.com>
2017-02-01 14:08:12 +0800

    跑板

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -33,8 +33,8 @@ $(function(){
 
   getPostsAjax = function(){
     $.get( "/getPosts", function( data ) {
-      $("#contentTmpl").tmpl(data.posts).appendTo(".post-clump");
       for (var key in data.posts){
+        $("#contentTmpl").tmpl(data.posts[key]).appendTo(".post-clump");
         getCommentsAjax(data.posts[key]._id);
       }
       $('.comment-btn').on('click', function(){
@@ -49,7 +49,7 @@ $(function(){
   getCommentsAjax = function(postId){
     $.get("/getComments/" + postId , function(data){
       console.log(data);
-      $("#commentTmpl").tmpl(data.comment).appendTo(".comment-area");
+      $("#commentTmpl").tmpl(data.comment).appendTo("#"+postId);
     })
 
   }
