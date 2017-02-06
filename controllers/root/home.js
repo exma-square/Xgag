d605dc9ced843bcc100785ea849a6e45755ebfa2
zpeng <cfps91152@gmail.com>
2017-02-06 14:00:21 +0800

    rename changeMeta & add google+ meta

--- controllers/root/home.js
+++ controllers/root/home.js
@@ -2,12 +2,19 @@ module.exports = function (req, res){
   if (req.session.flash.length > 0) {
     delete req.session.flash;
   }
-  var fbMeta = {
-    title: "Xgag",
-    url: config.domain,
-    image: config.domain + "images/fake_img/7.jpg",
-    description: "Xgag 是台灣的一個新聞平台，提供各種有趣, 好玩, 歡呼, 花惹發等新聞內容互動體驗，提供使用者全面且豐富的閱讀感受，我們是 Xgag。"
+  var changeMeta = {
+    fb: {
+      title: "Xgag",
+      url: config.domain,
+      image: config.domain + "images/xgaglogo.png",
+      description: "Xgag 是台灣的一個新聞平台，提供各種有趣, 好玩, 歡呼, 花惹發等新聞內容互動體驗，提供使用者全面且豐富的閱讀感受，我們是 Xgag。"
+    },
+    google: {
+      title: "Xgag",
+      image: config.domain + "images/xgaglogo.png",
+      description: "Xgag 是台灣的一個新聞平台，提供各種有趣, 好玩, 歡呼, 花惹發等新聞內容互動體驗，提供使用者全面且豐富的閱讀感受，我們是 Xgag。"
+    }
   }
-  res.render('index.jade', { title: 'Xgag', user: req.session.user, fbMeta: fbMeta});
+  res.render('index.jade', { title: 'Xgag', user: req.session.user, changeMeta: changeMeta});
 
 };
